75068003fcbc53c9580015681252601b53b9b15e
jacoboDev <jacobo.develop@gmail.com>
2026-02-18 19:23:56 +0100

    Fix dependencies

--- app/main.py
+++ app/main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
 #    allow_origins=["http://localhost:3000", "https://tu-frontend.com"],
+    allow_credentials=True,
     allow_methods=["*"],
 #    allow_methods=["GET", "POST"],
     allow_headers=["*"]
